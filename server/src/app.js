372bf114f42d2709918fca3d16caab6e284f8943
ArobyR <ar.rosario001@gmail.com>
2021-08-27 12:02:55 -0400

    add cors dependency and middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from "express"
 import mongoose from "mongoose"
 import config from "config"
+import cors from "cors"
 import userRoute from "./routes/users.js"
 import contactRoute from "./routes/contacts.js"
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 app.use("/api/users", userRoute);
 app.use("/api/contacts", contactRoute)
 
