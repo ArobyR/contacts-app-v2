c5b19783167696d8c82aa1312a86396976723129
ArobyR <ar.rosario001@gmail.com>
2021-08-28 19:11:19 -0400

    add middleware authorization and delete route

--- server/src/routes/contacts.js
+++ server/src/routes/contacts.js
@@ -1,25 +1,32 @@
 import express from "express";
+import tokenValidation from "../middlewares/auth.js"
 import {
   createContact,
+  deactivateContact,
   getContactsByUser,
   updateContact,
 } from "../services/contactService.js";
 
 const route = express.Router();
 
-route.get("/:id", async (req, res) => {
+route.get("/:id", tokenValidation, async (req, res) => {
   const phones = await getContactsByUser(req.params.id);
   res.json(phones);
 });
 
-route.post("/:id", async (req, res) => {
+route.post("/:id", tokenValidation, async (req, res) => {
   const result = await createContact(req.body, req.params.id);
   res.json(result);
 });
 
-route.put("/phone/:id", async (req, res) => {
+route.put("/phone/:id", tokenValidation, async (req, res) => {
   const result = await updateContact(req.params.id, req.body);
   res.json(result);
 });
+
+route.delete("/:id", tokenValidation, async (req, res) => {
+  const result = await deactivateContact(req.params.id)
+  res.json(result)
+})
 
 export default route;
