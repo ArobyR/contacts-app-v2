084c128c18958f899f446f7695bc728c01b05fcd
ArobyR <ar.rosario001@gmail.com>
2021-11-16 20:31:22 -0400

    modified components
    Add changes on App and Home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import  Button  from "@material-ui/core/Button"
+import React from "react";
+import Login from "./components/Login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import SignUp from "./components/SignUp/SignUp";
+import Home from "./components/Home/Home";
 
 const App = () => {
-    return (
-        <div>
-            <Button variant="outlined" color="primary">Hello world</Button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Contacts from '../Contacts/Contacts'
 import NavBar from '../NavBar/NavBar'
 
 const Home = () => {
     return (
-        <NavBar />
+        <React.Fragment>
+            <NavBar />
+            <Contacts />
+        </React.Fragment>
     )
 }
 
